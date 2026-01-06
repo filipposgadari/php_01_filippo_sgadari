c28ad5df770c905814bbe366c095f29bac9919f2
filipposgadari <filipposgadari97@gmail.com>
2026-01-06 14:57:01 +0100

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,11 @@
+<?php
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = '$text2';
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+print_r ("$text1 " . " $text2 " . "$text3 " . "$text4" . "$text5 " . "$text2 " . "$text7 " . "$text8 " . "$text4");
