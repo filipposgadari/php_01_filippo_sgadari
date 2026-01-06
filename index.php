71f0e71055f83063fb92b8221f1466d633cf2d54
filipposgadari <filipposgadari97@gmail.com>
2026-01-06 11:15:53 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = "Filippo";
+$eta = 28;
+$uomo = true;
+$altezza = 1.75;
+
+const name = "Filippo";
+const eta = 28;
+const uomo = true;
+const altezza = 1.75;
+
+var_dump ($name);
+var_dump ($eta);
+var_dump ($uomo);
+var_dump ($altezza);
