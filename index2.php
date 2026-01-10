d2cefb95d649f8512eed5bed11faf12194241c67
filipposgadari <filipposgadari97@gmail.com>
2026-01-11 00:19:25 +0100

    modificata variabile $text6

--- index2.php
+++ index2.php
@@ -8,4 +8,6 @@ $text6 = '$text2';
 $text7 = 'bevuto';
 $text8 = "tutto";
 
-print_r ("$text1 " . " $text2 " . "$text3 " . "$text4" . "$text5 " . "$text2 " . "$text7 " . "$text8 " . "$text4");
+$text_Concatenata  = $text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text5 . " " . $text2 . " " . $text7 . " " . $text8 . ". \n";
+
+echo $text_Concatenata;
